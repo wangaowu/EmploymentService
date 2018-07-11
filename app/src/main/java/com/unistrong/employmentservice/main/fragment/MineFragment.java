d15dd4c47b5fbc853ca407cebbbe71bc10dc3750
wangaowu <wang_gaowu@qq.com>
2018-07-11 17:40:27 +0800

    //给个人页签  加动画

--- app/src/main/java/com/unistrong/employmentservice/main/fragment/MineFragment.java
+++ app/src/main/java/com/unistrong/employmentservice/main/fragment/MineFragment.java
@@ -1,5 +1,6 @@
 package com.unistrong.employmentservice.main.fragment;
 
+import android.animation.ValueAnimator;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,7 +9,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 
+import com.unistrong.baselibs.utils.DensityUtils;
 import com.unistrong.baselibs.utils.SPUtils;
 import com.unistrong.employmentservice.R;
 import com.unistrong.employmentservice.databinding.FragmentMineBinding;
@@ -35,6 +38,22 @@ public class MineFragment extends Fragment implements View.OnClickListener {
     private void initData() {
         binding.tvName.setText(SPUtils.getString(getContext(), Constant.SP_KEY.USER_NAME));
         binding.tvOffice.setText(SPUtils.getString(getContext(), Constant.SP_KEY.USER_OFFICE));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        int animatorHeight = DensityUtils.dp2px(getContext(), 150);
+        binding.rlTopAnimator.setPadding(0, 0, 0, animatorHeight);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(animatorHeight, 0);
+        valueAnimator.addUpdateListener(animation -> {
+            int animatedValue = (int) animation.getAnimatedValue();
+            binding.rlTopAnimator.setPadding(0, 0, 0, animatedValue);
+            binding.rlTopAnimator.invalidate();
+        });
+        valueAnimator.setDuration(800);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.start();
     }
 
     @Override
