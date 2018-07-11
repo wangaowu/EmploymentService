227cb000e8eac59b367e721dfd634f00339e9b61
wangaowu <wang_gaowu@qq.com>
2018-07-11 17:14:55 +0800

    //bean恢复

--- app/src/main/java/com/unistrong/framwork/resp/ChartResp.java
+++ app/src/main/java/com/unistrong/framwork/resp/ChartResp.java
@@ -45,13 +45,6 @@ public class ChartResp {
     }
 
     public List<ResultBean> getResult() {
-        result = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            ResultBean resultBean = new ResultBean();
-            resultBean.setCurrentDayCount("1050" + i);
-            resultBean.setDay("2018-02-" + i);
-            result.add(resultBean);
-        }
         return result;
     }
 
